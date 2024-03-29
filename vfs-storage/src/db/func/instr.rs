3422d624cabc5f9ac81660aa9fdb30c0ca6f5be6
Noam Alffasy <7583207+noamalffasy@users.noreply.github.com>
2024-03-29 14:18:04 +0000

    Test instr function

--- vfs-storage/src/db/func/instr.rs
+++ vfs-storage/src/db/func/instr.rs
@@ -17,7 +17,7 @@ impl Iden for Instr {
 ///
 /// # Examples
 ///
-/// ```
+/// ```ignore
 /// use sea_query::{tests_cfg::*, *};
 ///
 /// let query = Query::select()
@@ -44,4 +44,46 @@ where
     B: Into<SimpleExpr>,
 {
     Func::cust(Instr).args([str.into(), substr.into()])
+}
+
+#[cfg(test)]
+mod tests {
+    use sea_query::{tests_cfg::*, *};
+
+    use super::instr;
+
+    fn build_query() -> SelectStatement {
+        return Query::select()
+            .expr(instr(Expr::col(Char::Character), Expr::expr(',')))
+            .from(Char::Table)
+            .to_owned();
+    }
+
+    #[test]
+    fn test_function_calling_in_mysql() {
+        let query = build_query();
+
+        assert_eq!(
+            query.to_string(MysqlQueryBuilder),
+            r#"SELECT INSTR(`character`, ',') FROM `character`"#
+        );
+    }
+    #[test]
+    fn test_function_calling_in_postgres() {
+        let query = build_query();
+
+        assert_eq!(
+            query.to_string(PostgresQueryBuilder),
+            r#"SELECT INSTR("character", ',') FROM "character""#
+        );
+    }
+    #[test]
+    fn test_function_calling_in_sqlite() {
+        let query = build_query();
+
+        assert_eq!(
+            query.to_string(SqliteQueryBuilder),
+            r#"SELECT INSTR("character", ',') FROM "character""#
+        );
+    }
 }
